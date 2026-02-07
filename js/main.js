f0db8b975330141782a25f57428ffde3b02fd4a8
its phestone <254267085+justabacha@users.noreply.github.com>
2026-02-07 13:48:54 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-import { initUI } from './ui.js';
-import { getWeather } from './weather.js';
-import { initQuotes } from './quotes.js';
-import { setupMagic } from './magic.js';
-import { initTimer } from './timer.js';
+import './ui.js';
+import './timer.js';
+import './music.js';
+import './magic.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  initUI();
-  getWeather();
-  initQuotes();
-  setupMagic();
-  initTimer();
+  if (window.initUI) window.initUI();
 });

719ad6a96463c74bf699fba2785f540a2019d32b
its phestone <254267085+justabacha@users.noreply.github.com>
2026-02-08 23:44:10 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'phestone-mission-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './quotes_feed.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});

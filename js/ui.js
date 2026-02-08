674e3537415d29452b54d6fd7b0cda86d3d18eb3
its phestone <254267085+justabacha@users.noreply.github.com>
2026-02-09 02:56:32 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,15 @@
 export const backgrounds = [
     "https://i.postimg.cc/JGsdX7Xt/1000481158.jpg",
     "https://i.postimg.cc/cvcd1qVN/7afe2684-b3e3-4a12-a80b-17531d44bad4.jpg"
+     "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1493246507139-91e8bef99c02?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1502082553048-f009c37129b9?auto=format&fit=crop&q=80&w=1000",
+    "https://images.unsplash.com/photo-1433086566608-571ad11350a9?auto=format&fit=crop&q=80&w=1000"
     // ... add your full list here
 ];
 
